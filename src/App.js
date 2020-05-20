444783f0a9ab644769c2969c19b658394e43c3d2
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-20 14:45:22 -0500

    add Search function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
   componentDidMount(){
@@ -16,9 +17,14 @@ class App extends React.Component {
       .then(users => this.setState({monsters: users}))
   }
   render(){
+    const { monsters, searchField} = this.state
+    const filterMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input type='search' placeholder='Search monster' 
+                onChange={e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters={filterMonsters} />
       </div>
   );
   }
