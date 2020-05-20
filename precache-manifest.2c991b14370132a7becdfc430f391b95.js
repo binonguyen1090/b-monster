9d95e86f891cdaa52a2512dd96b587a8ac8ea12e
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-20 15:36:16 -0500

    Updates

---
+++ precache-manifest.2c991b14370132a7becdfc430f391b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0259819879dcfab886e5d6de08600e59",
+    "url": "/b-monster/index.html"
+  },
+  {
+    "revision": "381c7fc381a3aa407c9e",
+    "url": "/b-monster/static/css/main.87e670c2.chunk.css"
+  },
+  {
+    "revision": "b0271f2fe6798469e5a7",
+    "url": "/b-monster/static/js/2.dde0386a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/b-monster/static/js/2.dde0386a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381c7fc381a3aa407c9e",
+    "url": "/b-monster/static/js/main.9aa30e9d.chunk.js"
+  },
+  {
+    "revision": "f87323050965a5d19bd7",
+    "url": "/b-monster/static/js/runtime-main.37c2c74c.js"
+  }
+]);
